d8da987577b4d41441a307e1588a17c32ca77a2d
vidhigoyal18 <vidhi.goyalb.tech188@gmail.com>
2021-11-23 17:29:08 +0530

    String method

---
+++ src/com/company/Palindromstring.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Palindromstring {
+    public static void main(String[]args){
+        int n,a,sum=0,r;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        a=n;
+        while(n>0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(a==sum)
+            System.out.println("Palindrome string");
+        else
+    System.out.println("Not Palindrome");
+    }
+}
